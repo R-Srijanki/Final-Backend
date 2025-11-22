e6944021427965a1b30b4207a15a4337c1cc2cd7
R-Srijanki <jankirathod999@gmail.com>
2025-11-22 23:07:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+mongoose.connect("mongodb+srv://jankirathod999_db_user:FTZX2f2LGjqYvuSo@cluster0.dcefapd.mongodb.net/");
+const db=mongoose.connection;
+db.on("open",()=>{
+    console.log("Connection successful");
+});
+
+db.on("error",()=>{
+    console.log("Connection not successful");
+});
+app.listen(8000,()=>{
+    console.log("Server listening on port 8000");
+})
+
+//jankirathod999_db_user
+//FTZX2f2LGjqYvuSo
