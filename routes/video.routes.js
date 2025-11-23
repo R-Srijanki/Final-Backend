c389c031f1aab958c13b1b8e5d08de11a956f164
R-Srijanki <jankirathod999@gmail.com>
2025-11-23 20:04:30 +0530

    video routes

--- routes/video.routes.js
+++ routes/video.routes.js
@@ -1,11 +1,25 @@
-import verifyToken from "../middlewares/verify";
+import {
+  getVideos,
+  getVideo,
+  uploadVideo,
+  updateVideo,
+  deleteVideo,
+  likeVideo,
+  dislikeVideo
+} from "../controllers/video.controller.js";
 
-export default function videoRoutes(app){
-    app.get('/videos',getVideos);
-    app.get('/videos/:id',getVideo);
-    app.post('/videos',verifyToken,uploadVideo);
-    app.put('/videos/:id',verifyToken,updateVideo);
-    app.delete('/videos/:id',verifyToken,deleteVideo);
-    app.post('/videos/:id/like',verifyToken,likeVideo);
-    app.post('videos/:id/dislike',verifyToken,dislikeVideo);
+import verifyToken from "../middlewares/verify.js";
+
+export default function videoRoutes(app) {
+  app.get("/videos", getVideos);
+  app.get("/videos/:id", getVideo);
+
+  app.post("/videos", verifyToken, uploadVideo);
+
+  app.put("/videos/:id", verifyToken, updateVideo);
+
+  app.delete("/videos/:id", verifyToken, deleteVideo);
+
+  app.post("/videos/:id/like", verifyToken, likeVideo);
+  app.post("/videos/:id/dislike", verifyToken, dislikeVideo);
 }
