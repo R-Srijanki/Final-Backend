20ebde14394c7e58d07557613c8e2623d0e4c51f
R-Srijanki <jankirathod999@gmail.com>
2025-11-24 17:31:25 +0530

    edit channel route

--- controllers/channel.controllers.js
+++ controllers/channel.controllers.js
@@ -57,11 +57,35 @@ export async function getChannel(req,res) {
 }
 export async function editChannel(req,res) {
     try{
-        
+        const { id } = req.params;
+
+        const channel = await Channel.findById(id);
+        if (!channel)
+        return res.status(404).json({ message: "Channel not found" });
+
+        if (channel.owner.toString() !== req.user._id.toString())
+        return res.status(403).json({ message: "Unauthorized" });
+
+        const updates = req.body;
+
+        const updatedChannel = await Channel.findByIdAndUpdate(
+        id,
+        { $set: updates },
+        { new: true }
+        );
+
+        return res.status(200).json({
+        message: "Channel updated successfully",
+        data: updatedChannel
+        });
+
     }
-    catch(err){
-        return res.status(500).json({message:"error occurred during channel edit",error:err.message});
-    }
+    catch (err) {
+        return res.status(500).json({
+            message: "Error updating channel",
+            error: err.message
+        });
+  }
 }
 export async function deleteChannel(req,res) {
     try{
